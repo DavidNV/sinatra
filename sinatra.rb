d703e6ff1a84f3acac67bfa86d4b7f789422d3f8
Orferus <david.nvalderrama@gmail.com>
2018-03-08 21:06:55 -0500

    Routes, sessions and something else

--- sinatra.rb
+++ sinatra.rb
@@ -1,6 +1,48 @@
 require 'sinatra'
 
-get '/' do
-  @nombre = params["nombre"] ? params["nombre"] : "desconocido!"
+enable :sessions
+
+get "/form" do
+  if session["name"] && session["last_name"]
+    @name = session["name"]
+    @last_name = session["last_name"]
+    erb :form_response
+  else
+    @session_closed = session["session_closed"]
+    erb :form
+  end
+end
+
+post "/form_response" do
+  @name = params["name"]
+  @last_name = params["last_name"]
+  session.store("name", @name)
+  session.store("last_name", @last_name)
+
+  erb :form_response
+end
+
+get "/log_out" do
+  session.delete("name")
+  session.delete("last_name")
+  session.store("session_closed", true)
+  redirect "/form"
+end
+
+get '/makers/:nombre' do
+  @nombre = params["nombre"].capitalize
   erb :index
+end
+
+get '/rectangle' do
+  width = params["width"].to_i
+  heigth = params["heigth"].to_i
+  area = width * heigth
+  "Rectangle Area: #{area}"
+end
+
+get '/square/:side' do
+  side = params["side"].to_i
+  area = side * side
+  "Square Area: #{area}"
 end
